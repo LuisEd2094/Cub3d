54532ac278b3d32109bbed4b6df8401afe05a73a
Luis <luissoto2094@gmail.com>
2024-01-11 10:39:43 +0100

    merged with parse_file, removed tests dir from check_file, passes norm but for main that has a for loop for printing debug info

--- src/check_file/tests/make_test.sh
+++
@@ -1,5 +0,0 @@
-make -C /Users/lsoto-do/core04/Cub3d/ &&  cc ./main.c ./unity.c  && ./a.out
-
-#With leaks
-
-#cc ./main.c ./unity.c ../*.c ../../aux/*.c  ../../../libft/libft.a  -I../../../includes -I./ -I../ && leaks -at -- ./a.out
